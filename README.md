23976ff0d164d79222d328d7ae6583f5a2ce0712
Maigre <thomas.bohl@gmail.com>
2015-09-02 11:40:02 +0200

    Initial commit with Segmenter

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # HLS-server
 Linux HLS Server including uploader, segmenter, chunks dealer and media manager
+
+This project is in early development status.
+
+Available features:
+## Segmenter
+
+
+## TODO
+= add BASEURL while creating m3u8 playlist
+= validate HLS flux compatibility
+
+= check if I-Frame can be optimized (at the beggining)
+  = monitor: http://stackoverflow.com/questions/14005110/how-to-split-a-video-using-ffmpeg-so-that-each-chunk-starts-with-a-key-frame/14011638#14011638
+  = After segementation: http://video.stackexchange.com/questions/4904/how-to-force-ffmpeg-to-insert-keyframe-at-first-frame-when-downsampling-a-framer
+  = alternative solution to segment: https://www.ffmpeg.org/ffmpeg-formats.html#Examples-5
---
+++ presets.py
@@ -0,0 +1,92 @@
+import os.path
+from collections import OrderedDict
+
+# SEGMENTS DURATION (seconds)
+SEGMENT_SIZE = 10
+
+# DEFAULT STREAM NAME
+STREAM_NAME = 'stream'
+
+# FFMPEG PRESET [ultrafast,superfast, veryfast, faster, fast, medium, slow, slower, veryslow]
+FFMPEG_PRESET = 'veryslow'
+
+# PRESETS FOR ADAPTATIVE QUALITY
+PROFILES = OrderedDict()
+PROFILES['0-ugly'] = {
+    'resolutions':  { '4/3': '400x300', '16/9': '416x234' },
+    'audiobitrate': '32k',
+    'videobitrate': '100k',
+    'buffersize':   '200k',
+    'profile':      'baseline',
+    'level':        '3.0',
+    'fps':          10
+}
+PROFILES['1-bad'] = {
+    'resolutions':  { '4/3': '400x300', '16/9': '416x234' },
+    'audiobitrate': '64k',
+    'videobitrate': '200k',
+    'buffersize':   '400k',
+    'profile':      'baseline',
+    'level':        '3.0',
+    'fps':          12
+}
+PROFILES['2-tiny'] = {
+    'resolutions':  { '4/3': '480x360', '16/9': '480x270' },
+    'audiobitrate': '64k',
+    'videobitrate': '400k',
+    'buffersize':   '800k',
+    'profile':      'baseline',
+    'level':        '3.0',
+    'fps':          15
+}
+PROFILES['3-low'] = {
+    'resolutions':  { '4/3': '640x480', '16/9': '640x360' },
+    'audiobitrate': '64k',
+    'videobitrate': '600k',
+    'buffersize':   '1200k',
+    'profile':      'baseline',
+    'level':        '3.0',
+    'fps':          24
+}
+PROFILES['4-medium'] = {
+    'resolutions':  { '4/3': '640x480', '16/9': '640x360' },
+    'audiobitrate': '96k',
+    'videobitrate': '1200k',
+    'buffersize':   '2400k',
+    'profile':      'baseline',
+    'level':        '3.1',
+    'fps':          24
+}
+PROFILES['5-high'] = {
+    'resolutions':  { '4/3': '960x720', '16/9': '960x540' },
+    'audiobitrate': '96k',
+    'videobitrate': '3500k',
+    'buffersize':   '7000k',
+    'profile':      'main',
+    'level':        '3.1',
+    'fps':          24
+}
+PROFILES['6-hd'] = {
+    'resolutions':  { '4/3': '1280x960', '16/9': '1280x720' },
+    'audiobitrate': '128k',
+    'videobitrate': '5000k',
+    'buffersize':   '10000k',
+    'profile':      'main',
+    'level':        '3.1',
+    'fps':          24
+}
+
+def build(inputfile, ratio='16/9', name=False, segmentsize=False, ffmpegmode=False):
+    presets = PROFILES.copy()
+    for quality, profile in presets.items():
+        try:
+            presets[quality]['resolution'] = profile['resolutions'][ratio]
+            presets[quality]['gop'] = presets[quality]['fps'] * 3
+            presets[quality]['inputfile'] = inputfile
+            presets[quality]['outputname'] = name if name else STREAM_NAME
+            presets[quality]['segmentsize'] = segmentsize if segmentsize else SEGMENT_SIZE
+            presets[quality]['ffmpegmode'] = ffmpegmode if ffmpegmode else FFMPEG_PRESET
+        except:
+            print 'Error while building {0} Profile with ratio {1}.. Ignoring'.format(quality, ratio)
+            del presets[quality]
+    return presets.items()
